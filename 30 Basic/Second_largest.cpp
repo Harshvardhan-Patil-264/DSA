25070aad4c8a3fabde0626b6321125620d559154
Harshvardhan Patil <91559701+Harshvardhan-Patil-264@users.noreply.github.com>
2025-07-25 18:48:57 +0530

    Update Second_largest.cpp

--- 30 Basic/Second_largest.cpp
+++ 30 Basic/Second_largest.cpp
@@ -1,26 +1,29 @@
-class Solution {
-  public:
-    int getSecondLargest(vector<int> &arr) {
-      
-      int n = arr.size();
-      
-      int largest = -1;
-      int slargest = -1;
-      
-      for(int i = 0; i < n; i++){
-          if(arr[i] > largest){
-              slargest = largest;
-              largest = arr[i];
-          }
-          
-          else if(arr[i] < largest && arr[i] > slargest){
-                slargest = arr[i];
-              }
-          
-      }
-      return slargest;
-        
-        
-        
+#include <bits/stdc++.h>
+using namespace std;
+
+void findSecondLargest(const vector<int> &arr) {
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int num : arr) {
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num > secondLargest && num != largest) {
+            secondLargest = num;
+        }
     }
-};
+
+    if (secondLargest == INT_MIN)
+        cout << "No second largest element (all elements might be same)." << endl;
+    else {
+        cout << "Largest: " << largest << endl;
+        cout << "Second Largest: " << secondLargest << endl;
+    }
+}
+
+int main() {
+    vector<int> arr = {3, 1, 7, 9, 2, 5}; 
+    findSecondLargest(arr);
+    return 0;
+}
